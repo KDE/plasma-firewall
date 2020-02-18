99462247d113e40a16bd7fa4b383f2534c53ffd3
Tomaz Canabrava <tcanabrava@kde.org>
2020-02-18 11:07:08 +0000

    Implement the UFW backend
    This uses the IFirewallClient interface to implement a backend
    to the UFW firewall, using KAuth to query for root access via
    polkit. The code is quite minimal as most of the things are correctly
    handled via the core library.

---
+++ kcm/backends/ufw/helper/helper.cpp
@@ -0,0 +1,355 @@
+/*
+ * UFW KControl Module
+ *
+ * Copyright 2011 Craig Drummond <craig.p.drummond@gmail.com>
+ *
+ * ----
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "helper.h"
+#include <QDebug>
+#include <QByteArray>
+#include <QProcess>
+#include <QProcessEnvironment>
+#include <QString>
+#include <QStringList>
+#include <QTextCodec>
+#include <QDir>
+#include <QFile>
+#include <sys/stat.h>
+
+#include <KAuth>
+#include <KAuthHelperSupport>
+
+#include "ufw_helper_config.h"
+
+namespace {
+constexpr int FILE_PERMS = 0644;
+constexpr int DIR_PERMS = 0755;
+const QString KCM_UFW_DIR = QStringLiteral("/etc/kcm_ufw");
+const QString PROFILE_EXTENSION = QStringLiteral(".ufw");
+const QString LOG_FILE = QStringLiteral("/var/log/ufw.log");
+
+void setPermissions(const QString &f, int perms)
+{
+    // Clear any umask before setting file perms
+    mode_t oldMask(umask(0000));
+    ::chmod(QFile::encodeName(f).constData(), perms);
+    // Reset umask
+    ::umask(oldMask);
+}
+
+void checkFolder()
+{
+    QDir d(KCM_UFW_DIR);
+
+    if(!d.exists()) {
+        d.mkpath(KCM_UFW_DIR);
+        setPermissions(d.absolutePath(), DIR_PERMS);
+    }
+}
+
+} // namespace
+
+namespace UFW {
+ActionReply Helper::query(const QVariantMap &args)
+{
+    qDebug() << __FUNCTION__;
+    ActionReply reply=args["defaults"].toBool()
+                        ? run({"--status", "--defaults", "--list", "--modules"}, "query")
+                        : run({"--status", "--list"}, "query");
+
+    if(args["profiles"].toBool()) {
+        QDir dir(KCM_UFW_DIR);
+        QStringList profiles=dir.entryList({"*" + PROFILE_EXTENSION });
+        QMap<QString, QVariant> data;
+        for (const QString &profile : profiles) {
+            QFile f(dir.canonicalPath()+QChar('/')+profile);
+            if (f.open(QIODevice::ReadOnly)) {
+                data.insert(profile, f.readAll());
+            }
+        }
+        reply.addData("profiles", data);
+    }
+
+    return reply;
+}
+
+ActionReply Helper::viewlog(const QVariantMap &args)
+{
+    qDebug() << __FUNCTION__;
+
+    QString     lastLine=args["lastLine"].toString(),
+                logFile=args["logFile"].toString();
+    QFile       file(logFile.isEmpty() ? LOG_FILE : logFile);
+    ActionReply reply;
+
+    if(!file.open(QIODevice::ReadOnly|QIODevice::Text)) {
+        return ActionReply::HelperErrorReply(STATUS_OPERATION_FAILED);
+    }
+
+    QStringList lines;
+    while (!file.atEnd()) {
+        QString line(file.readLine());
+
+        if(line.contains(" [UFW ")) {
+            if(!lastLine.isEmpty() && line==lastLine) {
+                lines.clear();
+                continue;
+            }
+            lines.append(line);
+        }
+    }
+
+    reply.addData("lines", lines);
+    return reply;
+}
+
+ActionReply Helper::modify(const QVariantMap &args)
+{
+    qDebug() << __FUNCTION__;
+    QString cmd=args["cmd"].toString();
+
+    // QProcess converts its args using QString().toLocal8Bit()!!!, so use UTF-8 codec.
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
+    if("setStatus"==cmd)
+        return setStatus(args, cmd);
+    else if("addRules"==cmd)
+        return addRules(args, cmd);
+    else if("removeRule"==cmd)
+        return removeRule(args, cmd);
+    else if("moveRule"==cmd)
+        return moveRule(args, cmd);
+    else if("editRule"==cmd)
+        return editRule(args, cmd);
+//     else if("editRuleDescr"==cmd)
+//         return editRuleDescr(args, cmd);
+    else if("reset"==cmd)
+        return reset(cmd);
+    else if("setDefaults"==cmd)
+        return setDefaults(args, cmd);
+    else if("setModules"==cmd)
+        return setModules(args, cmd);
+    else if("setProfile"==cmd)
+        return setProfile(args, cmd);
+    else if("saveProfile"==cmd)
+        return saveProfile(args, cmd);
+    else if("deleteProfile"==cmd)
+        return deleteProfile(args, cmd);
+
+    ActionReply reply=ActionReply::HelperErrorReply(STATUS_INVALID_CMD);
+    return reply;
+}
+
+ActionReply Helper::setStatus(const QVariantMap &args, const QString &cmd)
+{
+    const QString enabled = args["status"].toBool() ? "true" : "false";
+
+    return run({"--setEnabled=" + enabled},
+               {"--status"}, cmd);
+}
+
+ActionReply Helper::setDefaults(const QVariantMap &args, const QString &cmd)
+{
+    QStringList query({"--defaults"});
+    if (args["ipv6"].toBool())
+        query.append("--list");
+
+    const QString defaults = args["xml"].toString();
+
+    return run({"--setDefaults="+defaults},
+               query, cmd);
+}
+
+ActionReply Helper::setModules(const QVariantMap &args, const QString &cmd)
+{
+    return run({"--setModules="+args["xml"].toString()},
+               {"--modules"}, cmd);
+}
+
+ActionReply Helper::setProfile(const QVariantMap &args, const QString &cmd)
+{
+    QStringList cmdArgs;
+
+    if(args.contains("ruleCount"))
+    {
+        unsigned int count=args["ruleCount"].toUInt();
+
+        cmdArgs.append("--clearRules");
+        for(unsigned int i=0; i < count; ++i) {
+            const QString argument = args["rule"+QString::number(i)].toString();
+            cmdArgs.append("--add="+argument);
+        }
+    }
+
+    if(args.contains("defaults")) {
+        cmdArgs << "--setDefaults="+args["defaults"].toString();
+    }
+    if(args.contains("modules")) {
+        cmdArgs << "--setModules="+args["modules"].toString();
+    }
+
+    if(cmdArgs.isEmpty()) {
+        return ActionReply::HelperErrorReply(STATUS_INVALID_ARGUMENTS);
+    }
+
+    checkFolder();
+    return run(cmdArgs, {"--status", "--defaults", "--list", "--modules"}, cmd);
+}
+
+ActionReply Helper::saveProfile(const QVariantMap &args, const QString &cmd)
+{
+    qDebug() << __FUNCTION__ << args;
+
+    QString     name(args["name"].toString()),
+                xml(args["xml"].toString());
+    ActionReply reply;
+
+    if(name.isEmpty() || xml.isEmpty())
+    {
+        reply=ActionReply::HelperErrorReply(STATUS_INVALID_ARGUMENTS);
+    }
+    else
+    {
+        checkFolder();
+
+        QFile f(QString(KCM_UFW_DIR)+"/"+name+PROFILE_EXTENSION);
+
+        if(f.open(QIODevice::WriteOnly))
+        {
+            QTextStream(&f) << xml;
+            f.close();
+            setPermissions(f.fileName(), FILE_PERMS);
+        }
+        else
+        {
+            reply=ActionReply::HelperErrorReply(STATUS_OPERATION_FAILED);
+        }
+    }
+
+    reply.addData("cmd", cmd);
+    reply.addData("name", name);
+    reply.addData("profiles", QDir(KCM_UFW_DIR).entryList({"*" + PROFILE_EXTENSION }));
+    return reply;
+}
+
+ActionReply Helper::deleteProfile(const QVariantMap &args, const QString &cmd)
+{
+    qDebug() << __FUNCTION__ << args;
+
+    QString     name(args["name"].toString());
+    ActionReply reply;
+
+    if(name.isEmpty())
+    {
+        reply=ActionReply::HelperErrorReply(STATUS_INVALID_ARGUMENTS);
+    }
+    else if(!QFile::remove(QString(KCM_UFW_DIR)+"/"+name+PROFILE_EXTENSION))
+    {
+        reply=ActionReply::HelperErrorReply(STATUS_OPERATION_FAILED);
+    }
+
+    reply.addData("cmd", cmd);
+    reply.addData("name", name);
+    reply.addData("profiles", QDir(KCM_UFW_DIR).entryList({"*" +  PROFILE_EXTENSION }));
+    return reply;
+}
+
+ActionReply Helper::addRules(const QVariantMap &args, const QString &cmd)
+{
+    unsigned int count=args["count"].toUInt();
+
+    if(count>0)
+    {
+        QStringList cmdArgs;
+
+        for(unsigned int i=0; i<count; ++i)
+            cmdArgs << "--add="+args["xml"+QString::number(i)].toString();
+
+        checkFolder();
+        return run(cmdArgs, {"--list"}, cmd);
+    }
+    ActionReply reply=ActionReply::HelperErrorReply(STATUS_INVALID_ARGUMENTS);
+    return reply;
+}
+
+ActionReply Helper::removeRule(const QVariantMap &args, const QString &cmd)
+{
+    checkFolder();
+    return run({"--remove="+args["index"].toString()},
+               {"--list"}, cmd);
+}
+
+ActionReply Helper::moveRule(const QVariantMap &args, const QString &cmd)
+{
+    checkFolder();
+    const QString from = QString::number(args["from"].toUInt());
+    const QString to = QString::number(args["to"].toUInt());
+
+    return run({"--move="+ from + ':' + to },
+               {"--list"}, cmd);
+}
+
+ActionReply Helper::editRule(const QVariantMap &args, const QString &cmd)
+{
+    checkFolder();
+    return run({"--update="+args["xml"].toString()},
+               {"--list"}, cmd);
+}
+
+ActionReply Helper::reset(const QString &cmd)
+{
+    return run({"--reset"},
+               {"--status", "--defaults", "--list", "--modules"}, cmd);
+}
+
+ActionReply Helper::run(const QStringList &args, const QStringList &second, const QString &cmd)
+{
+    ActionReply reply=run(args, cmd);
+    if(0==reply.errorCode())
+        reply=run(second, cmd);
+    return reply;
+}
+
+ActionReply Helper::run(const QStringList &args, const QString &cmd)
+{
+    QProcess    ufw;
+    ActionReply reply;
+
+    qDebug() << __FUNCTION__ << args;
+    ufw.start(UFW_PLUGIN_HELPER_PATH, args, QIODevice::ReadOnly);
+    if (ufw.waitForStarted())
+        ufw.waitForFinished();
+
+    int exitCode(ufw.exitCode());
+
+    if(0!=exitCode)
+    {
+        reply=ActionReply::HelperErrorReply(exitCode);
+        reply.addData("response", ufw.readAllStandardError());
+    }
+    else
+        reply.addData("response", ufw.readAllStandardOutput());
+    reply.addData("cmd", cmd);
+    return reply;
+}
+
+}
+
+KAUTH_HELPER_MAIN("org.kde.ufw", UFW::Helper)
---
+++ kcm/backends/ufw/ufwclient.cpp
@@ -0,0 +1,605 @@
+/*
+ * Copyright 2018 Alexis Lopes Zubeta <contact@azubieta.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ufwclient.h"
+
+#include <QDebug>
+#include <QTimer>
+#include <QVariantMap>
+#include <QNetworkInterface>
+#include <QStandardPaths>
+#include <QDir>
+
+#include <KConfig>
+#include <KLocalizedString>
+#include <KConfigGroup>
+
+// TODO: Figure out what's wrong with the registering
+// REGISTER_BACKEND("ufw", UfwClient::createMethod);
+
+UfwClient::UfwClient(FirewallClient *parent) :
+    IFirewallClientBackend(parent),
+    m_isBusy(false),
+    m_rulesModel(new RuleListModel(this)),
+    m_logs(new LogListModel(this))
+{
+    // HACK: Quering the firewall status in this context
+    // creates a segmentation fault error in some situations
+    // due to an usage of the rootObject before it's
+    // initialization. So, it's delayed a little.
+    //    refresh();
+    QTimer::singleShot(100, this, &UfwClient::refresh);
+    QTimer::singleShot(2000, this, &UfwClient::refreshLogs);
+}
+
+QString UfwClient::name() const
+{
+    return QStringLiteral("ufw");
+}
+
+void UfwClient::refresh()
+{
+    queryStatus();
+}
+
+bool UfwClient::enabled() const
+{
+    return m_currentProfile.getEnabled();
+}
+
+void UfwClient::setEnabled(bool value)
+{
+    if (enabled() == value) {
+        return;
+    }
+
+    QVariantMap args {
+        {"cmd", "setStatus"},
+        {"status", value},
+    };
+
+    KAuth::Action modifyAction = buildModifyAction(args);
+
+    m_status = value ? i18n("Enabling the firewall...") : i18n("Disabling the firewall...");
+    m_isBusy = true;
+
+
+    KAuth::ExecuteJob *job = modifyAction.execute();
+    connect(job, &KAuth::ExecuteJob::result, [this] (KJob *kjob)
+    {
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+        setBusy(false);
+
+        if (!job->error())
+            queryStatus(true, false);
+    });
+
+    job->start();
+}
+
+
+bool UfwClient::isBusy() const
+{
+    return m_isBusy;
+}
+
+void UfwClient::queryStatus(bool readDefaults, bool listProfiles)
+{
+    if (isBusy())
+    {
+        qWarning() << "Ufw client is busy";
+        return;
+    }
+
+    QVariantMap args {
+        {"defaults", readDefaults},
+        {"profiles", listProfiles},
+    };
+
+    if (m_queryAction.name().isEmpty()) {
+        m_queryAction = buildQueryAction(args);
+    }
+
+    KAuth::ExecuteJob *job = m_queryAction.execute();
+    connect(job, &KAuth::ExecuteJob::result, [this] (KJob *kjob)
+    {
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+
+        if (!job->error())
+        {
+            QByteArray response = job->data().value("response", "").toByteArray();
+            setProfile(Profile(response));
+        } else {
+            setStatus(
+                QStringLiteral("There was an error in the backend! Please report it. \n") +
+                job->action().name() + QStringLiteral(" ") + job->errorString()
+            );
+            qWarning() << job->action().name() << job->errorString();
+        }
+
+        setBusy(false);
+    });
+
+    job->start();
+}
+
+void UfwClient::setDefaultIncomingPolicy(QString policy)
+{
+    if (policy == defaultIncomingPolicy()) {
+        return;
+    }
+
+    const QString xmlArg = QStringLiteral("<defaults incoming=\"%1\"/>").arg(policy);
+
+    QVariantMap args {
+        {"cmd","setDefaults"},
+        {"xml", xmlArg},
+    };
+
+    KAuth::Action modifyAction = buildModifyAction(args);
+    m_status = i18n("Setting firewall default incomming policy...");
+    m_isBusy = true;
+
+    KAuth::ExecuteJob *job = modifyAction.execute();
+    connect(job, &KAuth::ExecuteJob::result, [this] (KJob *kjob)
+    {
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+
+        setBusy(false);
+
+        if (!job->error())
+            queryStatus(true, false);
+
+
+    });
+
+    job->start();
+}
+
+void UfwClient::setDefaultOutgoingPolicy(QString policy)
+{
+    if (policy == defaultOutgoingPolicy()) {
+        return;
+    }
+
+    const QString xmlArg = QStringLiteral("<defaults outgoing=\"%1\"/>").arg(policy);
+
+    QVariantMap args {
+        {"cmd", "setDefaults"},
+        {"xml", xmlArg},
+    };
+
+    KAuth::Action modifyAction = buildModifyAction(args);
+    m_status = i18n("Setting firewall default outgoing policy...");
+    m_isBusy = true;
+
+    KAuth::ExecuteJob *job = modifyAction.execute();
+    connect(job, &KAuth::ExecuteJob::result, [this] (KJob *kjob)
+    {
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+
+        setBusy(false);
+
+        if (!job->error())
+            queryStatus(true, false);
+
+
+    });
+
+    job->start();
+}
+
+void UfwClient::setLogsAutoRefresh(bool logsAutoRefresh)
+{
+    if (m_logsAutoRefresh == logsAutoRefresh)
+        return;
+
+    if (logsAutoRefresh) {
+        connect(&m_logsRefreshTimer, &QTimer::timeout, this, &UfwClient::refreshLogs);
+        m_logsRefreshTimer.setInterval(3000);
+        m_logsRefreshTimer.start();
+    } else {
+        disconnect(&m_logsRefreshTimer, &QTimer::timeout, this, &UfwClient::refreshLogs);
+        m_logsRefreshTimer.stop();
+    }
+
+    m_logsAutoRefresh = logsAutoRefresh;
+    emit parentClient()->logsAutoRefreshChanged(m_logsAutoRefresh);
+}
+
+void UfwClient::refreshLogs()
+{
+    KAuth::Action action("org.kde.ufw.viewlog");
+    action.setHelperId("org.kde.ufw");
+
+    QVariantMap args;
+    if (m_rawLogs.size() > 0)
+        args["lastLine"] = m_rawLogs.last();
+
+    action.setArguments(args);
+
+    KAuth::ExecuteJob *job = action.execute();
+    connect(job, &KAuth::ExecuteJob::finished, [this] (KJob *kjob)
+    {
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+
+        if (!job->error())
+        {
+            QStringList newLogs = job->data().value("lines", "").toStringList();
+            m_rawLogs.append(newLogs);
+            m_logs->addRawLogs(newLogs);
+        } else {
+            qWarning() << "org.kde.ufw.viewlog" << job->errorString();
+        }
+        setBusy(false);
+    });
+
+    job->start();
+}
+
+void UfwClient::setStatus(const QString &status)
+{
+    m_status = status;
+    emit parentClient()->statusChanged(m_status);
+}
+
+void UfwClient::setBusy(const bool &isBusy)
+{
+    if (m_isBusy != isBusy)
+    {
+        m_isBusy = isBusy;
+        emit parentClient()->isBusyChanged(isBusy);
+    }
+}
+
+void UfwClient::setProfile(Profile profile)
+{
+    auto oldProfile = m_currentProfile;
+    m_currentProfile = profile;
+
+    m_rulesModel->setProfile(m_currentProfile);
+    if (m_currentProfile.getEnabled() != oldProfile.getEnabled())
+        emit parentClient()->enabledChanged(m_currentProfile.getEnabled());
+
+    if (m_currentProfile.getDefaultIncomingPolicy() != oldProfile.getDefaultIncomingPolicy()) {
+        QString policy = Types::toString(m_currentProfile.getDefaultIncomingPolicy());
+        emit parentClient()->defaultIncomingPolicyChanged(policy);
+    }
+
+    if (m_currentProfile.getDefaultOutgoingPolicy() != oldProfile.getDefaultOutgoingPolicy()) {
+        QString policy = Types::toString(m_currentProfile.getDefaultOutgoingPolicy());
+        emit parentClient()->defaultOutgoingPolicyChanged(policy);
+    }
+}
+
+KAuth::Action UfwClient::buildQueryAction(const QVariantMap &arguments)
+{
+    auto action = KAuth::Action("org.kde.ufw.query");
+    action.setHelperId("org.kde.ufw");
+    action.setArguments(arguments);
+
+    return action;
+}
+
+KAuth::Action UfwClient::buildModifyAction(const QVariantMap &arguments)
+{
+    auto action = KAuth::Action("org.kde.ufw.modify");
+    action.setHelperId("org.kde.ufw");
+    action.setArguments(arguments);
+
+    return action;
+}
+
+QString UfwClient::status() const
+{
+    return m_status;
+}
+
+RuleListModel *UfwClient::rules() const
+{
+    return m_rulesModel;
+}
+
+RuleWrapper *UfwClient::getRule(int index)
+{
+    auto rules = m_currentProfile.getRules();
+
+    if (index < 0 || index >= rules.count()) {
+        return nullptr;
+    }
+
+    auto rule = rules.at(index);
+    rule.setPosition(index);
+    RuleWrapper * wrapper = new RuleWrapper(rule, this);
+
+    return wrapper;
+}
+
+void UfwClient::addRule(RuleWrapper *ruleWrapper)
+{
+    if (ruleWrapper == nullptr) {
+        qWarning() << __FUNCTION__ << "nullptr rule";
+        return;
+    }
+
+    Rule rule = ruleWrapper->getRule();
+
+    QVariantMap args {
+        {"cmd", "addRules"},
+        {"count",1},
+        {"xml0", rule.toXml()},
+    };
+
+    KAuth::Action modifyAction = buildModifyAction(args);
+
+    KAuth::ExecuteJob *job = modifyAction.execute();
+    connect(job, &KAuth::ExecuteJob::result, [this] (KJob *kjob)
+    {
+        setBusy(false);
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+        if (!job->error())
+        {
+            queryStatus();
+        } else {
+            qWarning() << job->action().name() << job->errorString();
+        }
+    });
+
+    job->start();
+}
+
+void UfwClient::removeRule(int index)
+{
+    if (index < 0 || index >= m_currentProfile.getRules().count()) {
+        qWarning() << __FUNCTION__ << "invalid rule index";
+        return;
+    }
+
+    // Correct index
+    index += 1;
+
+    QVariantMap args {
+        {"cmd", "removeRule"},
+        {"index", QString::number(index)},
+    };
+
+    KAuth::Action modifyAction = buildModifyAction(args);
+    KAuth::ExecuteJob *job = modifyAction.execute();
+    connect(job, &KAuth::ExecuteJob::result, [this] (KJob *kjob)
+    {
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+
+        if (!job->error())
+        {
+            queryStatus();
+        } else {
+            qWarning() << job->action().name() << job->errorString();
+        }
+        setBusy(false);
+    });
+
+    job->start();
+}
+
+void UfwClient::updateRule(RuleWrapper *ruleWrapper)
+{
+    if (ruleWrapper == nullptr) {
+        qWarning() << __FUNCTION__ << "nullptr rule";
+        return;
+    }
+
+    Rule rule = ruleWrapper->getRule();
+
+    rule.setPosition(rule.getPosition() + 1);
+    QVariantMap args {
+        {"cmd", "editRule"},
+        {"xml", rule.toXml()},
+    };
+
+    KAuth::Action modifyAction = buildModifyAction(args);
+    KAuth::ExecuteJob *job = modifyAction.execute();
+    connect(job, &KAuth::ExecuteJob::result, [this] (KJob *kjob)
+    {
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+
+        if (!job->error())
+        {
+            queryStatus();
+        } else {
+            qWarning() << job->action().name() << job->errorString();
+        }
+        setBusy(false);
+    });
+
+    job->start();
+}
+
+void UfwClient::moveRule(int from, int to)
+{
+    QList<Rule> rules = m_currentProfile.getRules();
+    if (from < 0 || from >= rules.count()) {
+        qWarning() << __FUNCTION__ << "invalid from index";
+        return;
+    }
+
+    if (to < 0 || to >= rules.count()) {
+        qWarning() << __FUNCTION__ << "invalid to index";
+        return;
+    }
+    // Correct indices
+    from += 1;
+    to += 1;
+
+    QVariantMap args {
+        {"cmd", "moveRule"},
+        {"from", from},
+        {"to", to},
+    };
+
+    KAuth::Action modifyAction = buildModifyAction(args);
+    KAuth::ExecuteJob *job = modifyAction.execute();
+    connect(job, &KAuth::ExecuteJob::finished, [this] (KJob *kjob)
+    {
+        auto job = qobject_cast<KAuth::ExecuteJob *>(kjob);
+
+        if (!job->error())
+        {
+            queryStatus();
+        } else {
+            qWarning() << job->action().name() << job->errorString();
+        }
+        setBusy(false);
+    });
+
+    job->start();
+}
+
+QString UfwClient::defaultIncomingPolicy() const
+{
+    auto policy_t = m_currentProfile.getDefaultIncomingPolicy();
+    return Types::toString(policy_t);
+}
+
+QString UfwClient::defaultOutgoingPolicy() const
+{
+    auto policy_t = m_currentProfile.getDefaultOutgoingPolicy();
+    return Types::toString(policy_t);
+}
+
+LogListModel *UfwClient::logs()
+{
+    return m_logs;
+}
+
+
+bool UfwClient::logsAutoRefresh() const
+{
+    return m_logsAutoRefresh;
+}
+
+RuleWrapper* UfwClient::createRuleFromConnection(const QString &protocol, const QString &localAddress, const QString &foreignAddres, const QString &status)
+{
+    auto _localAddress = localAddress;
+    _localAddress.replace("*", "");
+    _localAddress.replace("0.0.0.0", "");
+
+    auto _foreignAddres = foreignAddres;
+    _foreignAddres.replace("*", "");
+    _foreignAddres.replace("0.0.0.0", "");
+
+    auto localAddressData = _localAddress.split(":");
+    auto foreignAddresData = _foreignAddres.split(":");
+
+    auto rule = new RuleWrapper({});
+    rule->setIncoming(status == QStringLiteral("LISTEN"));
+    rule->setPolicy("deny");
+
+    // Prepare rule draft
+    if (status == QStringLiteral("LISTEN")) {
+        rule->setSourceAddress(foreignAddresData[0]);
+        rule->setSourcePort(foreignAddresData[1]);
+        rule->setDestinationAddress(localAddressData[0]);
+        rule->setDestinationPort(localAddressData[1]);
+    } else {
+        rule->setSourceAddress(localAddressData[0]);
+        rule->setSourcePort(localAddressData[1]);
+        rule->setDestinationAddress(foreignAddresData[0]);
+        rule->setDestinationPort(foreignAddresData[1]);
+    }
+
+    rule->setProtocol(FirewallClient::getKnownProtocols().indexOf(protocol.toUpper()));
+    return rule;
+}
+
+RuleWrapper *UfwClient::createRuleFromLog(
+    const QString &protocol,
+    const QString &sourceAddress,
+    const QString &sourcePort,
+    const QString &destinationAddress,
+    const QString &destinationPort,
+    const QString &inn)
+{
+        // Transform to the ufw notation
+        auto rule = new RuleWrapper({});
+
+        auto _sourceAddress = sourceAddress;
+        _sourceAddress.replace("*", "");
+        _sourceAddress.replace("0.0.0.0", "");
+
+        auto _destinationAddress = destinationAddress;
+        _destinationAddress.replace("*", "");
+        _destinationAddress.replace("0.0.0.0", "");
+
+        // Prepare rule draft
+        rule->setIncoming(inn.size());
+        rule->setPolicy("allow");
+        rule->setSourceAddress(_sourceAddress);
+        rule->setSourcePort(sourcePort);
+
+        rule->setDestinationAddress(_destinationAddress);
+        rule->setDestinationPort(destinationPort);
+
+        rule->setProtocol(FirewallClient::getKnownProtocols().indexOf(protocol.toUpper()));
+        return rule;
+}
+
+IFirewallClientBackend* UfwClient::createMethod(FirewallClient* parent)
+{
+    IFirewallClientBackend *instance = new UfwClient(parent);
+    return instance;
+}
+
+bool UfwClient::hasExecutable() const {
+     return !QStandardPaths::findExecutable("ufw").isEmpty();
+}
+ 
+void UfwClient::setExecutable(const bool &hasExecutable){
+    emit parentClient()->hasExecutableChanged(hasExecutable);
+}
+
+void UfwClient::refreshProfiles()
+{
+
+    QList<Entry> profiles;
+    static const char * constProfileDir="/etc/ufw/applications.d/";
+
+    QStringList                files(QDir(constProfileDir).entryList());
+    QStringList::ConstIterator it(files.constBegin()),
+                                end(files.constEnd());
+
+    for(; it!=end; ++it) {
+        if((*it)!="." && (*it)!="..") {
+            KConfig                    cfg(constProfileDir+(*it), KConfig::SimpleConfig);
+            QStringList                groups(cfg.groupList());
+            QStringList::ConstIterator gIt(groups.constBegin()),
+                                        gEnd(groups.constEnd());
+
+            for(; gIt!=gEnd; ++gIt)
+            {
+                QString ports(cfg.group(*gIt).readEntry("ports", QString()));
+
+                if(!ports.isEmpty() && !profiles.contains(*gIt))
+                    profiles.append(Entry(*gIt, ports));
+            }
+        }
+    }
+
+    setProfiles(profiles);
+}
