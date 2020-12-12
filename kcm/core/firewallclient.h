c9f0d2aebcc3d5eef14ae53ff41c2f03835927bf
Lucas Biaggi <lbjanuario@gmail.com>
2020-12-12 08:42:17 -0300

    remove unnecessary ipvfamily enum

--- kcm/core/firewallclient.h
+++ kcm/core/firewallclient.h
@@ -51,8 +51,6 @@ class KCM_FIREWALL_CORE_EXPORT FirewallClient : public QObject
 public:
     enum DefaultDataBehavior { DontReadDefaults, ReadDefaults };
     enum ProfilesBehavior { DontListenProfiles, ListenProfiles };
-    enum Ipv { IPV4, IPV6 };
-
     explicit FirewallClient(QObject *parent = nullptr);
 
     Q_INVOKABLE static QStringList knownProtocols();
